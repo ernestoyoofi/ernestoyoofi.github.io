5b594db3ff01674d5127b146f62568020afa057a
Ernestoyoofi <yoofi.ernesto@gmail.com>
2021-12-18 13:20:39 +0700

    create new file

---
+++ script/get-code/get-display-1.js
@@ -0,0 +1,12 @@
+{
+/* Whatsapp Log Bot */
+document.getElementById("title-wa").innerHTML = "Matematika Bersama Jerome";
+document.getElementById("member-wa").innerHTML = "12 Perserta" ;
+document.getElementById("member-out-wa").innerHTML = "3 Peserta";
+document.getElementById("member-in-wa").innerHTML = "1 Peserta";
+/* Discord Log Bot */
+document.getElementById("title-discord").innerHTML = "Ernesto Studio Community, Inc.";
+document.getElementById("member-discord").innerHTML = "40 Perserta" ;
+document.getElementById("member-out-discord").innerHTML = "0 Peserta";
+document.getElementById("member-in-discord").innerHTML = "7 Peserta";
+}
