37c79f558bbdefcb39e59a54bbf457903bd03f72
Ernestoyoofi <yoofi.ernesto@gmail.com>
2021-12-19 06:29:22 +0700

    database

--- script/get-code/get-display-1.js
+++ script/get-code/get-display-1.js
@@ -3,7 +3,7 @@
 document.getElementById("title-wa").innerHTML = "Ernesto Community";
 document.getElementById("member-wa").innerHTML = "12 Perserta" ;
 document.getElementById("member-out-wa").innerHTML = "3 Peserta";
-document.getElementById("member-in-wa").innerHTML = "1 Peserta";
+document.getElementById("member-in-wa").innerHTML = "2 Peserta";
 /* Discord Log Bot */
 document.getElementById("title-discord").innerHTML = "Ernesto Studio Community, Inc.";
 document.getElementById("member-discord").innerHTML = "40 Perserta" ;
